d075584f3cc2d7785b928dbbe711df90564fc101
Farus <velikanov.exe@gmail.com>
2024-08-10 15:09:09 +0300

    Add project files.

---
+++ FuhUstal/FuhUstalMod.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuhUstal
+{
+    [BepInPlugin(modGuid, modName, modVersion)]
+    public class FuhUstalMod : BaseUnityPlugin
+    {
+        private const string modGuid = "com.fuhustal";
+        private const string modName = "FuhUstal";
+        private const string modVersion = "1.0.0";
+
+        private readonly HarmonyLib.Harmony harmony = new HarmonyLib.Harmony(modGuid);
+
+        private static FuhUstalMod instance;
+        internal ManualLogSource logSource;
+        void Awake()
+        {
+            if (instance != null)
+            {
+                Destroy(this);
+                return;
+            }
+
+            logSource = BepInEx.Logging.Logger.CreateLogSource(modGuid);
+            
+            logSource.LogInfo("FuhUstalMod has been loaded!");
+
+            harmony.PatchAll(typeof(FuhUstalMod));
+            harmony.PatchAll(typeof(Patches.PlayerControllerBPatch));
+        }
+    }
+}
---
+++ FuhUstal/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+using GameNetcodeStuff;
+
+namespace FuhUstal.Patches
+{
+    [HarmonyLib.HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyLib.HarmonyPatch("Update")]
+        [HarmonyLib.HarmonyPostfix]
+        static void UstalSound(ref float ___sprintMeter)
+        {
+            
+              ___sprintMeter = 1;
+            
+        }
+    }
+}
